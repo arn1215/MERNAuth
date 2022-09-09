5838b6c3d921ce474a5e3d1aad473554746c0f0f
Ali <ali.naqvi.dev@gmail.com>
2022-09-09 07:26:27 -0500

    forms are now controlled component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import { useState } from 'react'
+
+const inputStyle = 'border-2 border-blue-500 py-2 px-3 rounded-sm w-full'
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const login = () => {
+    const user = {
+      password, email, 
+    }
+    console.log(user)
+  }
   return (
-    <div>
-    <div className='flex justify-center items-center h-screen space-y-5 flex-col'>
-      
-      <input type='text' placeholder='username' className='border-2 border-gray-500 py-2 px-5'/>
-      <input type='text' placeholder='password' className='border-2 border-gray-500 py-2 px-5'/>
-      <button className="bg-blue-500 py-2 px-5 text-white">LOGIN</button>
+    <div className='flex justify-center items-center h-screen'>
+
+      <div className='w-[500px] space-y-5 flex-col p-5 shadow-lg border border-gray-300'>
+        <h1 className='font-semibold text-3xl text-gray-700'>Welcome Back</h1>
+        <input type='text' placeholder='email' className={inputStyle} onChange={e => setEmail(e.target.value)} value={email}/>
+        <input type='text' placeholder='password' className={inputStyle} onChange={e => setPassword(e.target.value)} value={password}/>
+        
+        <div className='flex justify-between items-end'>
+          <Link className="underline" to='/register'>Click Here To Register</Link>
+          <button className='py-1 px-5 text-white bg-blue-700' onClick={login}>LOGIN</button>
+        </div>
+
+      </div>
 
     </div>
-  </div>
   )
 }
 
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,23 +1,35 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const inputStyle = 'border-2 border-blue-500 py-2 px-3 rounded-sm w-full'
 
 function Register() {
+  const [email, setEmail] = useState("")
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const register = () => {
+    const newUser = {
+      userName, password, email, confirmPassword
+    }
+    console.log(newUser)
+  }
   return (
     <div className='flex justify-center items-center h-screen'>
 
-      <div className='w-[500px] space-y-5'>
+      <div className='w-[500px] space-y-5 flex-col p-5 shadow-lg border border-gray-300'>
         <h1 className='font-semibold text-3xl text-gray-700'>Welcome to MERN AUTH APP</h1>
-        <input type='text' placeholder='username' className={inputStyle}/>
-        <input type='text' placeholder='email' className={inputStyle}/>
-        <input type='text' placeholder='password' className={inputStyle}/>
-        <input type='text' placeholder='confirm password' className={inputStyle}/>
-        
+        <input type='text' placeholder='email' className={inputStyle} onChange={e => setEmail(e.target.value)} value={email} />
+        <input type='text' placeholder='username' className={inputStyle} onChange={e => setUserName(e.target.value)} value={userName} />
+        <input type='text' placeholder='password' className={inputStyle} onChange={e => setPassword(e.target.value)} value={password} />
+        <input type='text' placeholder='confirm password' className={inputStyle} onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} />
+
         <div className='flex justify-between items-end'>
           <Link className="underline" to='/login'>Click Here To Login</Link>
-          <button className='py-1 px-5 text-white bg-blue-700'>REGISTER</button>
+          <button className='py-1 px-5 text-white bg-blue-700' onClick={register}>REGISTER</button>
         </div>
-        
+
       </div>
 
     </div>
